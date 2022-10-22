cfddbeba45d08e5389372f6bf3fea4b109a63ddb
Akbar <akbarbadmus07@gmail.com>
2022-10-22 20:53:39 +0100

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-indigo-600">HNG Internship</h1>
+      <h1 className="text-red-600">HNG Internship</h1>
+    </div>
+  );
+}
+
+export default App;
